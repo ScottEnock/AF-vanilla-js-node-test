543c98969b05cc87330ed8fe6d48e933f39dec73
ScottEnock <27223098+ScottEnock@users.noreply.github.com>
2021-05-14 17:28:38 +0100

    IMplement initial front-end

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
-const test = () => console.log("testing TETETss great bgsasda");
+// form.addEventListener("submit", (e) => e.preventDefault());
+const axios = require("axios");
 
-test();
+document.addEventListener("DOMContentLoaded", () => {
+	const form = document.querySelector("#search");
+
+	form.addEventListener("submit", async (e) => {
+		e.preventDefault();
+		const input = document.querySelector("#country-input");
+		const ul = document.querySelector("ul");
+		ul.innerHTML = "";
+
+		const { data } = await axios
+			.get(`/countries/${input.value}.json`)
+			.catch(() => {
+				const li1 = document.createElement("li");
+				li1.innerHTML = `Could not match "${input.value}" within our records. Please try another search term.`;
+				ul.appendChild(li1);
+			});
+
+		data.forEach((x) => {
+			const li = document.createElement("li");
+			li.innerHTML = `${x.countryName} - ${x.capitalCity}`;
+			ul.appendChild(li);
+		});
+
+		input.value = "";
+	});
+});
